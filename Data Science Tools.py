1e50aecec81768096bc67a1e19d4cb44907e91c1
MDheeraj2002 <107130737+MDheeraj2002@users.noreply.github.com>
2023-05-18 13:45:37 +0530

    Add files via upload

---
+++ Data Science Tools.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Arthematic Operators
+
+# In[3]:
+
+
+x=5
+y=8
+z=4
+a=x+y+z
+b=x+y+z
+c=x*y*z
+print(a)
+print(b)
+print(c)
+
+
+# Introduction
+
+# Data Science Languages
+
+# 1.python
+# 2.SQL
+# 3.Java
+# 4.R programming
+# 5.Machine learining
+
+# Data Science Libraries
+
+# 1. Numpy
+# 2. Pandas
+# 3. sklearn
+# 4. pytorch
+# 5. scipy
+
+# In[4]:
+
+
+x=5
+y=8
+z=4
+a=x+y+z
+c=x*y*z
+print(a)
+print(c)
+
+
+# Objectives of jupyter notebook
+
+# Jupyter has the access of somany programming languages like python,R programming,SQL etc 
+# The main objective of juypter notebook is for programming and mostly used for data science appplication.
+# It is also used for data vizualization and model training using machine learing and R .
+
+# In[21]:
+
+
+total_minutes = 130
+
+# Get hours with floor division
+hours = total_minutes // 60
+
+# Get additional minutes with modulus
+minutes = total_minutes % 60
+
+# Create time as a string
+time = "{}:{}".format(hours, minutes)
+
+print(time)
+
+
+# In[ ]:
+
+
+
+
