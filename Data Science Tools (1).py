5d5f82039b3860cefa3f978df1825e6cb9cb1d44
MDheeraj2002 <107130737+MDheeraj2002@users.noreply.github.com>
2023-05-18 14:56:44 +0530

    Add files via upload

---
+++ Data Science Tools (1).py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Arthematic Operators
+
+# In[3]:
+
+
+x=5
+y=8
+z=4
+a=x+y+z
+b=x+y+z
+c=x*y*z
+print(a)
+print(b)
+print(c)
+
+
+# Introduction
+
+# Data Science Languages
+
+# + python
+# - SQL
+# * Java
+# + R programming
+# - Machine learining
+
+# Data Science Libraries
+
+# + pandas
+# - numpy
+# * pytorch
+# + Tensor flow
+# - sklearn
+# * scipy
+
+# Data Science Tools
+
+# S.No | Subject| Tools
+# :-|:-:|-:
+# **language**|*Data Science*|Python
+# **language**|*data Science*|SAS
+# **language**|*data Science*|SPSS
+# **language**|*data Science*|Mathlab
+# **language**|*data Science*|Rapid miner
+# **language**|*data Science*|Apache Hadoop
+# **language**|*data Science*|KNIME
+# 
+# 
+# 
+
+# In[4]:
+
+
+x=5
+y=8
+z=4
+a=x+y+z
+c=x*y*z
+print(a)
+print(c)
+
+
+# Objectives of markdown cell
+
+# + You can run Markdown cells in the same way that you can run code cells. 
+# - However, when you run a Markdown cell, the text formatted using Markdown syntax will be rendered as stylized text. 
+# * This means that headings are larger and bold, bulleted lists have bullets next to them instead of * , and regular text looks normal.
+
+# In[21]:
+
+
+total_minutes = 130
+
+# Get hours with floor division
+hours = total_minutes // 60
+
+# Get additional minutes with modulus
+minutes = total_minutes % 60
+
+# Create time as a string
+time = "{}:{}".format(hours, minutes)
+
+print(time)
+
+
+# Data Science Tools
+
+# In[ ]:
+
+
+
+
